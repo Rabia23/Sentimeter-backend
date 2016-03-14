afcf2f064d5764fc3416d87da37e2aef6d4e7f61
aamish <aamish.iftikhar@arbisoft.com>
2016-03-14 15:22:18 +0500

    Updates: Feedback CSV

--- apps/review/management/commands/gen_csv.py
+++ apps/review/management/commands/gen_csv.py
@@ -13,9 +13,14 @@ class Command(BaseCommand):
         file = open('eggs.csv','w')
         try:
             writer = csv.writer(file)
-            writer.writerow(('Feedback Id', 'Comment', 'Action Taken', 'gro'))
+            writer.writerow(('Feedback Id', 'Comment', 'Action Taken', 'GRO', 'Branch', 'Options'))
             for feedback in Feedback.objects.all():
-                writer.writerow((feedback.id, feedback.comment, feedback.action_taken, ))
+                writer.writerow((feedback.id,
+                                 feedback.comment,
+                                 feedback.action_taken,
+                                 feedback.gro_name,
+                                 feedback.branch.name,
+                                 [feedback_option.option.text for feedback_option in feedback.feedback_option.all()]))
         finally:
             file.close()
 
