9ee008a300a6d78d94b7f91cb794bcfc94022789
aamish <aamish.iftikhar@arbisoft.com>
2016-03-15 11:39:54 +0500

    Updates: Updated csv generation file

--- apps/review/management/commands/gen_csv.py
+++ apps/review/management/commands/gen_csv.py
@@ -10,17 +10,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        file = open('eggs.csv','w')
+        file = open('feedback.csv','w')
         try:
             writer = csv.writer(file)
             writer.writerow(('Feedback Id', 'Comment', 'Action Taken', 'GRO', 'Branch', 'Options'))
             for feedback in Feedback.objects.all():
+                self.stdout.write("Feedback ID = " + str(feedback.id))
                 writer.writerow((feedback.id,
                                  feedback.comment,
                                  feedback.action_taken,
                                  feedback.gro_name,
                                  feedback.branch.name,
-                                 [str(feedback_option.option.text.decode('utf-8')) for feedback_option in feedback.feedback_option.all()]))
+                                 [str(feedback_option.option.text) for feedback_option in feedback.feedback_option.all()]))
         finally:
             file.close()
 
