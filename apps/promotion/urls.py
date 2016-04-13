34644842b764c332e9e1c69ded5e522c856b99ed
aamish <aamish.iftikhar@arbisoft.com>
2016-04-13 11:51:21 +0500

    updates: address changes

--- apps/promotion/urls.py
+++ apps/promotion/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 from apps.promotion import views
 
 urlpatterns = patterns('',
-                       url(r'^promotion/$', views.PromotionView.as_view()),
+                       url(r'^promotion$', views.PromotionView.as_view()),
                        url(r'^promotion_question/$', views.PromotionQuestionsView.as_view()),
                        url(r'^promotion_add/$', views.PromotionAddView.as_view()),
                        )
--- apps/questionnaire/urls.py
+++ apps/questionnaire/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 from apps.questionnaire import views
 
 urlpatterns = patterns('',
-                       url(r'^questionnaire/$', views.QuestionnaireView.as_view()),
+                       url(r'^questionnaire$', views.QuestionnaireView.as_view()),
                        url(r'^questionnaire_question/$', views.QuestionnaireQuestionsView.as_view()),
                        url(r'^questionnaire_add/$', views.QuestionnaireAddView.as_view()),
                        )
