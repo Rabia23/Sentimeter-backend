4fba83554ef119c79a626803320800e383e89458
rabia23 <rabia.iftikhar@arbisoft.com>
2016-05-24 17:05:34 +0500

    added api for patch analysis  in reports.

--- apps/report/views.py
+++ apps/report/views.py
@@ -17,46 +17,28 @@ from apps.utils import response_json, get_user_data, get_param
 from django.utils.decorators import method_decorator
 from datetime import datetime, timedelta
 from apps import constants
+from apps.region.models import Region
 
 # Create your views here.
 class ReportView(APIView):
 
     def _get_complaint_view(self, date_from, date_to, region_id, city_id, branch_id):
-        complaint_view_list = []
+        data_list = []
+        objects = Region.objects.all()
 
-        feedback = Feedback.manager.date(date_from, date_to).filters(region_id, city_id, branch_id)
-        filtered_feedback = feedback.values('action_taken').annotate(count=Count('action_taken'))
-        filtered_feedback = generate_missing_actions(filtered_feedback)
-
-        data = {'feedback_count': feedback.count(), 'action_analysis': filtered_feedback}
-        complaint_view_list.append({'object': {"id": "", "name": "Pakistan", "objectId": ""}, 'data': data})
-
-        for object in Area.objects.all():
-            feedback = Feedback.manager.date(date_from, date_to).related_filters(0, object).filters(region_id, city_id, branch_id)
+        for object in objects:
+            feedback = Feedback.manager.related_filters(constants.REGIONAL_ANALYSIS, object).date(date_from, date_to)
             filtered_feedback = feedback.values('action_taken').annotate(count=Count('action_taken'))
             filtered_feedback = generate_missing_actions(filtered_feedback)
 
-            for feedback_dict in filtered_feedback:
-                feedback_dict.update({'color_code': constants.COLORS_ACTION_STATUS[feedback_dict["action_taken"]]})
-
             data = {'feedback_count': feedback.count(), 'action_analysis': filtered_feedback}
-            complaint_view_list.append({'object': ObjectSerializer(object).data, 'data': data})
+            data_list.append({'object': ObjectSerializer(object).data, 'data': data})
 
-        return complaint_view_list
+        return {'count': objects.count(), 'analysis': data_list}
 
-    def _get_top_concers(self):
+    def _get_top_concerns(self):
         concerns = [concern.to_dict() for concern in Concern.objects.filter(is_active=True).order_by("-count")[:5]]
         return {'concern_count': len(concerns), 'concern_list': concerns}
-
-    def _get_top_segment(self, date_from, date_to, region_id, city_id, branch_id):
-        question = Question.objects.get(type=constants.TYPE_1)
-
-        options = question.options.all()
-        feedback_options = FeedbackOption.manager.options(options).date(date_from, date_to)
-
-        feedback_segmented_list = generate_segmentation(feedback_options)
-        feedback_segmented_counts = [segment["option_count"] for segment in feedback_segmented_list]
-        return feedback_segmented_list[feedback_segmented_counts.index(max(feedback_segmented_counts))]
 
     def _get_overall_feedback(self, date_from, date_to, region_id, city_id, branch_id):
         feedback_options = FeedbackOption.manager.question(constants.TYPE_1).date(date_from, date_to).filters(region_id, city_id, branch_id)
@@ -80,40 +62,6 @@ class ReportView(APIView):
             children_feedback_segmented_list = generate_segmentation_with_options(children_feedback_options, children_options)
             sub_options_segments_list.append({'sub_option_segments_list': children_feedback_segmented_list})
         return {'segment_count': len(feedback_segmented_list), 'segments': feedback_segmented_list, 'sub_options_segments': sub_options_segments_list}
-
-    def _get_top_rankings(self, region_id, city_id, branch_id, date_from=None, date_to=None,):
-        overall_experience = FeedbackOption.get_top_option(date_from, date_to)
-        positive_negative_feedback = Feedback.get_feedback_type_count(date_from, date_to)
-        qsc_count = FeedbackOption.get_qsc_count(date_from, date_to)
-
-        return {'overall_experience': overall_experience,
-                'top_segment': self._get_top_segment(date_from, date_to, region_id, city_id, branch_id),
-                'positive_negative_feedback': positive_negative_feedback,
-                'qsc_count': qsc_count}
-
-    def _get_overall_rating(self, date_from, date_to, region_id, city_id, branch_id):
-        feedback_records_list = []
-
-        current_tz = timezone.get_current_timezone()
-        date_from = current_tz.localize(datetime.strptime(date_from + " 00:00:00", constants.DATE_FORMAT))
-        date_to = current_tz.localize(datetime.strptime(date_to + " 23:59:59", constants.DATE_FORMAT))
-
-        rule = rrule.DAILY
-        question = Question.objects.get(type=constants.TYPE_2)
-        for single_date in rrule.rrule(rule, dtstart=date_from, until=date_to):
-            feedback_options = FeedbackOption.manager.date(str(single_date.date()), str(single_date.date())).filters(region_id, city_id, branch_id)
-            feedback_options = feedback_options.question_parent_options(question)
-            filtered_feedback = feedback_options.values('option_id', 'option__text', 'option__parent_id', 'option__color_code').\
-                                    annotate(count=Count('option_id'))
-            list_feedback = generate_missing_options(question, filtered_feedback)
-
-            date_data = {'feedback_count': feedback_options.count(), 'feedbacks': list_feedback}
-            feedback_records_list.append({'date': single_date.date(), 'data': date_data})
-
-        if len(feedback_records_list) > constants.NO_OF_DAYS:
-            feedback_records_list = feedback_records_list[-constants.NO_OF_DAYS:]
-
-        return feedback_records_list
 
     def _get_opportunity_analysis(self, date_from, date_to, region_id, city_id, branch_id):
         feedback_options = FeedbackOption.manager.question(constants.TYPE_3).date(date_from, date_to).filters(region_id, city_id, branch_id)
@@ -148,6 +96,27 @@ class ReportView(APIView):
 
         return {'feedback_count': feedback_options.count(), 'feedbacks': list_feedback}
 
+    def _get_patch_analysis(self, date_from, date_to, region_id, city_id, branch_id):
+        question_type = [constants.TYPE_1, constants.TYPE_2]
+        question_feedbacks = []
+
+        for type in question_type:
+            feedback_options = FeedbackOption.manager.question(type).date(date_from, date_to)
+            region_objects = Region.objects.all()
+            feedbacks = []
+            for object in region_objects:
+                related_feedback_options = feedback_options.related_filters(constants.REGIONAL_ANALYSIS, object)
+                filtered_feedback_options = related_feedback_options.values('option_id', 'option__text', 'option__parent_id', 'option__color_code').annotate(count=Count('option_id'))
+                list_feedback = generate_missing_options(Question.objects.get(type=type), filtered_feedback_options)
+
+                data = {'feedback_count': related_feedback_options.count(), 'feedbacks': list_feedback}
+                feedbacks.append({'object': ObjectSerializer(object).data, 'data': data})
+            question_feedbacks.append({'question_feedbacks': feedbacks})
+        return {'count': region_objects.count(), 'analysis': question_feedbacks}
+
+
+
+
     @method_decorator(my_login_required)
     def get(self, request, user, format=None):
         try:
@@ -159,14 +128,13 @@ class ReportView(APIView):
 
             data = {
                 "segmentation_rating": self._get_segmentation_rating(date_from_str, date_to_str, region_id, city_id, branch_id),
-                "overall_feedback": self._get_overall_feedback(date_from_str, date_to_str, region_id, city_id, branch_id),
-                "overall_rating": self._get_overall_rating(str((now - timedelta(days=constants.NO_OF_DAYS)).date()), date_to_str, region_id, city_id, branch_id),
+                "overall_rating": self._get_overall_feedback(date_from_str, date_to_str, region_id, city_id, branch_id),
+                "patch_analysis": self._get_patch_analysis(date_from_str, date_to_str, region_id, city_id, branch_id),
                 "complaint_view": self._get_complaint_view(date_from_str, date_to_str, region_id, city_id, branch_id),
-                "top_rankings": self._get_top_rankings(date_from_str, date_to_str, region_id, city_id, branch_id),
-                "concerns": self._get_top_concers(),
-                "strength": self._get_opportunity_analysis(date_from_str, date_to_str, region_id, city_id, branch_id),
-                "customer_analysis": self._get_customer_analysis(date_from_str, date_to_str, region_id, city_id, branch_id),
-                "recommendation_analysis": self._get_recommendation_analysis(date_from_str, date_to_str, region_id, city_id, branch_id),
+                "concerns": self._get_top_concerns(),
+                "opportunity_analysis": self._get_opportunity_analysis(date_from_str, date_to_str, region_id, city_id, branch_id),
+                "age_group_analysis": self._get_customer_analysis(date_from_str, date_to_str, region_id, city_id, branch_id),
+                "recommendation_analysis": self._get_recommendation_analysis(date_from_str, date_to_str, region_id, city_id, branch_id)
             }
 
             return Response(response_json(True, data, None))
