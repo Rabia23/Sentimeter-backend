d8cd24c2b468b7149f20a17a4f06aac54df77a1d
rabia23 <rabia.iftikhar@arbisoft.com>
2016-06-08 15:10:50 +0500

    add new table and add table field in feedback table.

---
+++ apps/review/migrations/0014_feedback_table.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('table', '__first__'),
+        ('review', '0013_feedback_is_emailed'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='feedback',
+            name='table',
+            field=models.ForeignKey(blank=True, null=True, related_name='table', to='table.Table'),
+        ),
+    ]
