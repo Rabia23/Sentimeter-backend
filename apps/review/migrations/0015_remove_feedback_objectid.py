f9b69c987bdfb32fa582bbe135646baf6eef958a
aamish <aamish.iftikhar@arbisoft.com>
2016-06-10 12:49:22 +0500

    conflicts resolved

--- apps/review/migrations/0015_remove_feedback_objectid.py
+++ apps/review/migrations/0015_remove_feedback_objectid.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('review', '0014_feedback_table'),
+        ('review', '0011_feedback_segment'),
     ]
 
     operations = [
