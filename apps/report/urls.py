14c552b31f34161220c04eafa1deab5a622c88be
rabia23 <rabia.iftikhar@arbisoft.com>
2016-07-20 14:43:07 +0500

    add trailing slash at the end of report api url.

--- apps/report/urls.py
+++ apps/report/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, url
 from apps.report import views
 
 urlpatterns = patterns('',
-                       url(r'^report$', views.ReportView.as_view()),
+                       url(r'^report/$', views.ReportView.as_view()),
                        )
