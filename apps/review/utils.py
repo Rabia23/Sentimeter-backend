cdfd2c42cfa897669eb39e5f5d7447b721568620
rabia23 <rabia.iftikhar@arbisoft.com>
2016-07-28 17:50:06 +0500

    add batch feedback api.

--- apps/review/utils.py
+++ apps/review/utils.py
@@ -1,6 +1,15 @@
+from lively.mailgun import sendEmaiLMailGun
 from apps import constants
 from apps.review.enum import ActionStatusEnum
-from apps.utils import make_request
+from apps.utils import make_request, response_json
+from lively._celery import send_negative_feedback_email
+from apps.review.serializers import FeedbackSerializer
+from apps.person.utils import get_related_user
+from apps.review.models import FeedbackOption
+from lively import settings
+from rest_framework.response import Response
+from pytz import timezone
+from datetime import datetime
 
 __author__ = 'aamish'
 
@@ -8,6 +17,70 @@ __author__ = 'aamish'
 def feedback_get(object_id):
     response = make_request('GET', "application/json", '/1/classes/Feedback/%s' % object_id, '')
     return response
+
+
+def get_converted_time(date):
+    return datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f+00").replace(tzinfo=timezone('UTC'))
+
+
+def save_feedback(data):
+    if "user" in data:
+        user_data = data["user"]
+        user = get_related_user(user_data)
+    else:
+        user = None
+
+    feedback_params = data
+    feedback_params['user'] = user.id if user else None
+
+    serializer = FeedbackSerializer(data=data)
+    if serializer.is_valid():
+        feedback = serializer.save()
+        if feedback:
+            if "options" in data:
+                options_list = data["options"]
+
+                for option_id in options_list:
+                    if "created_at" in data:
+                        feedback_option = FeedbackOption(feedback=feedback, option_id=option_id)
+                        feedback_option.save()
+                        date = get_converted_time(data["created_at"])
+                        feedback_option.save_date(date)
+                    else:
+                        FeedbackOption(feedback=feedback, option_id=option_id).save()
+
+            if "created_at" in data:
+                date = get_converted_time(data["created_at"])
+                feedback.save_date(date)
+
+            feedback.mark_feedback_status()
+            feedback.keyword_analysis()
+            feedback.mark_segment()
+
+            if feedback.is_negative() and feedback.not_empty():
+                feedback.mark_for_report()
+                feedback_json = {
+                    "is_bad": feedback.is_bad(),
+                    "branch_name": feedback.branch.name,
+                    "branch_id": feedback.branch.id,
+                    "city_name": feedback.branch.city.name,
+                    "customer_name": feedback.customer_name(),
+                    "customer_phone": feedback.customer_phone(),
+                    "customer_email": feedback.customer_email(),
+                    "problems": feedback.problems(),
+                    "comment": feedback.comment,
+                    "table": feedback.get_table(feedback.id),
+                    "party_size": feedback.get_party_size(feedback.id),
+                    "server_link": settings.server_url,
+                    "time": feedback.created_at.strftime("%Y-%m-%d %H:%M:%S")
+                }
+
+                # send_negative_feedback_email(feedback_json)
+                # send_negative_feedback_email.delay(feedback_json)
+                sendEmaiLMailGun(feedback_json)
+
+            return True
+    return False
 
 
 def generate_missing_actions(data):
--- apps/review/views.py
+++ apps/review/views.py
@@ -7,16 +7,14 @@ from apps.option.models import Option
 from apps.option.utils import option_get, get_related_option
 from apps.person.utils import user_get, get_related_user
 from apps.review.models import Feedback, FeedbackOption
-from apps.review.serializers import FeedbackSerializer, FeedbackSearchSerializer
-from lively import settings
-from lively._celery import send_negative_feedback_email
+from apps.review.serializers import FeedbackSerializer
 from apps import constants
 from apps.utils import save, response, response_json, get_data_param
 from apps.redis_queue import RedisQueue
 from rest_framework.mixins import ListModelMixin
 from drf_haystack.generics import HaystackGenericAPIView
 from django.db import IntegrityError, transaction
-from lively.mailgun import sendEmaiLMailGun
+from apps.review.utils import save_feedback
 
 
 class FeedbackView(APIView):
@@ -28,56 +26,28 @@ class FeedbackView(APIView):
 
     @transaction.atomic
     def post(self, request, format=None):
-        if "user" in request.data:
-            user_data = request.data["user"]
-            user = get_related_user(user_data)
-        else:
-            user = None
+        status = save_feedback(request.data)
+        if status:
+            q = RedisQueue('feedback_redis_queue')
+            q.put(str(get_live_record()))
+            return Response(response_json(True, None, "Feedback successfully added"))
 
-        feedback_params = request.data
-        feedback_params['user'] = user.id if user else None
-
-        serializer = FeedbackSerializer(data=request.data)
-        if serializer.is_valid():
-            feedback = serializer.save()
-            if feedback:
-                if "options" in request.data:
-                    options_list = request.data["options"]
-
-                    for option_id in options_list:
-                        FeedbackOption(feedback=feedback, option_id=option_id).save()
-
-                feedback.mark_feedback_status()
-                feedback.keyword_analysis()
-                feedback.mark_segment()
-
-                # q = RedisQueue('feedback_redis_queue')
-                # q.put(str(get_live_record()))
-                # q.put("ping")
-
-                if feedback.is_negative() and feedback.not_empty():
-                    feedback.mark_for_report()
-                    feedback_json = {
-                        "is_bad": feedback.is_bad(),
-                        "branch_name": feedback.branch.name,
-                        "branch_id": feedback.branch.id,
-                        "city_name": feedback.branch.city.name,
-                        "customer_name": feedback.customer_name(),
-                        "customer_phone": feedback.customer_phone(),
-                        "customer_email": feedback.customer_email(),
-                        "problems": feedback.problems(),
-                        "comment": feedback.comment,
-                        "table": feedback.get_table(feedback.id),
-                        "party_size": feedback.get_party_size(feedback.id),
-                        "server_link": settings.server_url,
-                    }
-
-                    # send_negative_feedback_email(feedback_json)
-                    # send_negative_feedback_email.delay(feedback_json)
-                    sendEmaiLMailGun(feedback_json)
-
-                return Response(response_json(True, None, None))
         return Response(response_json(False, None, constants.TEXT_OPERATION_UNSUCCESSFUL))
+
+
+class FeedbackBatchView(APIView):
+
+    @transaction.atomic
+    def post(self, request, format=None):
+        feedback_array = request.data
+        for feedback in feedback_array:
+            status = save_feedback(feedback)
+            if status == False:
+                return Response(response_json(False, None, constants.TEXT_OPERATION_UNSUCCESSFUL))
+        q = RedisQueue('feedback_redis_queue')
+        q.put(str(get_live_record()))
+        # q.put("ping")
+        return Response(response_json(True, None, "Feedback successfully added"))
 
 
 
