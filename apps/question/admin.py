b360581b4b8e4c15e1ccb4232a4a0881b0fb5d53
zamanafzal <zamanafzal@hotmail.com>
2016-08-02 16:53:40 +0500

    feedback api

--- apps/question/admin.py
+++ apps/question/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Question,Questionnaire
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+class QuestionnaireAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Questionnaire, QuestionnaireAdmin)
--- apps/review/admin.py
+++ apps/review/admin.py
@@ -1,20 +1,21 @@
 # from api.views import DataView
-# from feedback.models import Feedback, Question, Option
-# from django.conf.urls import patterns
-# from django.contrib import admin
-# from django.http import HttpResponse
+from .models import Feedback, FeedbackOption
+from django.conf.urls import patterns
+from django.contrib import admin
+from django.http import HttpResponse
+
 #
-#
-# class FeedbackAdmin(admin.ModelAdmin):
-#     list_display = ('comment',)
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
 #
 #
 # class QuestionAdmin(admin.ModelAdmin):
 #     list_display = ('text',)
 #
 #
-# class OptionAdmin(admin.ModelAdmin):
-#     list_display = ('text',)
+class FeedbackOptionAdmin(admin.ModelAdmin):
+    list_display = ["feedback","option"]
+    pass
 #
 #
 # def get_admin_urls(urls):
@@ -25,9 +26,9 @@
 #         return my_urls + urls
 #     return get_urls
 #
-# admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
 # admin.site.register(Question, QuestionAdmin)
-# admin.site.register(Option, OptionAdmin)
+admin.site.register(FeedbackOption, FeedbackOptionAdmin)
 #
 # admin_urls = get_admin_urls(admin.site.get_urls())
 # admin.site.get_urls = admin_urls
