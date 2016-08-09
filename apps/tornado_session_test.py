e3a339588e59fc770b1f012063367fc5d645cce7
zamanafzal <zamanafzal@hotmail.com>
2016-08-09 11:18:07 +0500

    tornadao changes redis test

--- apps/tornado_session_test.py
+++ apps/tornado_session_test.py
@@ -1,39 +1,47 @@
-# -*- coding: utf-8 -*-
-
-"""
-Regular expression groups in URL pattern are passed as arguments to "open"
-method of tornado.websocket.WebSocketHandler
-"""
 import os
 import sys
 
-# Setup Django environment so that we can access Django models
-sys.path.append("~/Desktop/codes/sentimeter-backend/")
+# sys.path.append("~/Desktop/codes/sentimeter-backend/")
+sys.path.append("~/var/www/ginsoy/sentimeter-backend/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lively.settings")
+from apps.redis_queue import RedisQueue
 from tornado.web import Application
 from tornado.websocket import WebSocketHandler
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 import time
-
+from tornado.options import options, define, parse_command_line
 class SampleWebSocket(WebSocketHandler):
     clients = []
-    print("yes came in")
+    userid = 1
+
+    print("yes came in",)
     print(len(clients))
     def check_origin(self, origin):
         return True
     def open(self, userid):
+        length = 0
+        q = RedisQueue('feedback_redis_queue')
         self.clients.append(self)
         print("connection_initiated")
         print(self.clients)
         print(len(self.clients))
-        # while True:
-        #     print("true")
-        #     print(self.clients)
-        #     print(len(self.clients))
-        #
-        #     time.sleep(5)
-        self.write_message("%s" % userid)
+        abc = q.seek()
+        print("1",q)
+        print("2",abc)
+        print("3",abc[0])
+        data = abc[0].decode("utf-8")
+        print(str(data))
+        while True:
+            if length < q.qsize():
+                length = q.qsize()
+                print("true")
+                print(self.clients)
+                print(len(self.clients))
+
+
+                self.write_message("%s" % str(data))
+                time.sleep(10)
     def on_close(self):
         self.clients.remove(self)
         self.write_message("connection_closed")
@@ -45,7 +53,8 @@ app = Application([
 if __name__ == '__main__':
     http_server = HTTPServer(app)
     print("b")
-    http_server.bind(8888)
+    # http_server.bind(8888)
+    http_server.bind(5678)
     http_server.start(0)
     print("starting")
     IOLoop.current().start()
